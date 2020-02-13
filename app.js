f2a5e3b52eb35bfe490e12a2fdced1342f5d1b13
iprod5 <franklinluther3@gmail.com>
2020-02-13 14:31:26 +0000

    node init

---
+++ app.js
@@ -0,0 +1,13 @@
+// load the http module
+
+var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello to mmy Jenkins Test! \n");
+});
+
+// thiis will listen on localhost:9000
+server.listen(9000);
+console.log("Server listening on port 8000 :  http://127.0.0.1:9000/");
